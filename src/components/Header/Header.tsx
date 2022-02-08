b4e9da5c1a1c3686d2d2532929ac4cb0269b2ba9
Samarskiy.ivan <sa20201996@mail.ru>
2022-02-08 06:05:50 +0100

    update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,14 @@ import {NavLink} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {initialAuthStateType, logout} from "../../Redux/auth-reducer";
 import {RootStateType} from "../../Redux/store";
-
+import logo from "../../assets/images/logoApp.png"
 
 export function Header() {
     const dispatch = useDispatch()
     const auth = useSelector<RootStateType, initialAuthStateType>(state => state.auth)
     return (
         <header className={s.headerBlock}>
-            <img src="https://logodownload.org/wp-content/uploads/2017/05/marvel-logo-0.png"/>
+            <img src={logo}/>
             <div className={s.navLogin}>
                 {auth.isAuth
                     ? <div>{auth.email}
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,25 +6,25 @@ export function Nav(){
     return(
       <nav className={s.navBlock}>
           <div>
-              <NavLink to={"profile"} style={({ isActive }) => ({ color: isActive ? 'red' : 'white' })}>Profile</NavLink >
+              <NavLink to={"profile"} style={({ isActive }) => ({ color: isActive ? '#666666' : 'white' })}>Profile</NavLink >
           </div>
           <div>
-              <NavLink to={"messages"} style={({ isActive }) => ({ color: isActive ? 'red' : 'white' })}>Messages</NavLink>
+              <NavLink to={"messages"} style={({ isActive }) => ({ color: isActive ? '#666666' : 'white' })}>Messages</NavLink>
           </div>
           <div>
-              <NavLink to={"news"}  style={({ isActive }) => ({ color: isActive ? 'red' : 'white' })}>News</NavLink>
+              <NavLink to={"news"}  style={({ isActive }) => ({ color: isActive ? '#666666' : 'white' })}>News</NavLink>
           </div>
           <div>
-              <NavLink to={"music"}  style={({ isActive }) => ({ color: isActive ? 'red' : 'white' })}>Music</NavLink>
-          </div>
-          <br/>
-          <br/>
-          <div>
-              <NavLink to={"users"} style={({ isActive }) => ({ color: isActive ? 'red' : 'white' })}>Users</NavLink>
+              <NavLink to={"music"}  style={({ isActive }) => ({ color: isActive ? '#666666' : 'white' })}>Music</NavLink>
           </div>
           <br/>
+          <br/>
           <div>
-              <NavLink to={"settings"} style={({ isActive }) => ({ color: isActive ? 'red' : 'white' })}>Settings</NavLink>
+              <NavLink to={"users"} style={({ isActive }) => ({ color: isActive ? '#666666' : 'white' })}>Users</NavLink>
+          </div>
+          <br/>
+          <div>
+              <NavLink to={"settings"} style={({ isActive }) => ({ color: isActive ? '#666666' : 'white' })}>Settings</NavLink>
           </div>
       </nav>
     )
